d272204cb18b8965e846ec35b78e54713b332be8
HopoZ <2166632799@qq.com>
2026-03-15 22:18:31 +0800

    缓存套餐代码开发

--- star-server/src/main/java/com/star/StarApplication.java
+++ star-server/src/main/java/com/star/StarApplication.java
@@ -3,11 +3,13 @@ package com.star;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement //开启注解方式的事务管理
 @Slf4j
+@EnableCaching
 public class StarApplication {
     public static void main(String[] args) {
         SpringApplication.run(StarApplication.class, args);
